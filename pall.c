42fee74edeffa7e6a4c2f15a7263d93cc56d9a70
SlimjimmyMax <SlimjimmyMax@outlook.com>
2023-08-24 20:54:06 +0300

    0x19. C - Stacks, Queues - LIFO, FIFO

--- pall.c
+++ pall.c
@@ -6,15 +6,15 @@
  */
 void ej_pall(stack_t **head, unsigned int counter)
 {
-    stack_t *h;
-    (void) counter;
+	stack_t *h;
+	(void) counter;
 
-    h = *head;
-    if (h == NULL)
-        return;
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-    }
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 }
