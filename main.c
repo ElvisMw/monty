d1dbf774da25b52b7bb24500f4778a2bd6e99f5b
root <root@DESKTOP-DMRT987>
2023-08-24 23:06:36 +0300

    code fx

--- main.c
+++ main.c
@@ -1,10 +1,67 @@
 #include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
  * custom_getline - Custom implementation of the getline function.
  * @lineptr: Pointer to the line buffer.
  * @n: Pointer to the buffer size.
  * @stream: Pointer to the input stream.
  * Return: Number of characters read.
+ */
+ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
+{
+	size_t bufsize = 1024;
+	size_t pos = 0;
+	int c;
+
+	if (*lineptr == NULL)
+	{
+		*lineptr = (char *)malloc(bufsize);
+		if (*lineptr == NULL)
+		{
+			return (-1);
+		}
+		*n = bufsize;
+	}
+
+	while (1)
+	{
+		c = fgetc(stream);
+
+		if (c == EOF || c == '\n')
+		{
+			(*lineptr)[pos] = '\0';
+			break;
+		}
+		{
+			(*lineptr)[pos] = c;
+		}
+
+		pos++;
+
+		if (pos >= *n)
+		{
+			bufsize += 1024;
+			*lineptr = (char *)realloc(*lineptr, bufsize);
+			if (*lineptr == NULL)
+			{
+				return (-1);
+			}
+			*n = bufsize;
+		}
+	}
+
+	if (pos == 0 && c == EOF)
+	{
+		return (-1);
+	}
+
+	return (pos);
+}
+/**
+ * main - Entry point of the Monty interpreter.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ * Return: 0 on success.
  */
 int main(int argc, char *argv[])
 {
@@ -20,31 +77,25 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
+	bus.file = file;
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	bus_t bus = {file, NULL, NULL, 0};
-
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
+		read_line = custom_getline(&content, &size, file);
 		bus.content = content;
 		counter++;
-
 		if (read_line > 0)
 		{
 			execute(content, &stack, counter, file);
 		}
-
 		free(content);
 	}
-
 	free_stack(stack);
 	fclose(file);
 	return (0);
